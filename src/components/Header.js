f3a263f87e109f950aa7e4b2d5d50486ff068dcb
jjayeon <autumn.chiu.136@gmail.com>
2021-09-23 15:56:30 -0400

    CSS tidying

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,17 +5,17 @@ export default function Header() {
         <>
             <h1 className="breadcrumbs-title">Breadcrumbs</h1>
             <h5 className="breadcrumbs-subtitle">Help keep our ant alive!</h5>
-            <p className="user-instructions">
-                Each step, the ant will move forward.
-            </p>
-            <p>
-                If it sees white, it will turn right, and if it sees grey, it
-                will turn left.
-            </p>
-            <p>
-                Try to avoid the red patches, and guide the ant towards the
-                goal!
-            </p>
+            <div className="user-instructions">
+                <p>Each step, the ant will move forward.</p>
+                <p>
+                    If it sees white, it will turn right, and if it sees grey,
+                    it will turn left.
+                </p>
+                <p>
+                    Try to avoid the red patches, and guide the ant towards the
+                    goal!
+                </p>
+            </div>
         </>
     );
 }
